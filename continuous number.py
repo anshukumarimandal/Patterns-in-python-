32ca41313645ba48d9be3aa28024794929309876
Anshukumari Mandal <anshukumarimandal09@gmail.com>
2026-03-26 12:08:13 +0530

    Add files via upload

---
+++ continuous number.py
@@ -0,0 +1,8 @@
+#continous numbers pattern
+rows = int(input("Enter the number of rows:"))
+num = 1
+for i in range(1, rows + 1):
+    for j in range(i):
+        print(num, end=" ")
+        num += 1
+    print()
